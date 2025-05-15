2b964fa3eb742bd4cb03de216029d145bd06164d
bincybabu <bincysoja2017@gmail.com>
2025-05-15 10:52:12 +0530

    testing project

---
+++ database/seeders/TransactionSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
+
+class TransactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('transation')->insert([
+            'cust_id'         => "2",
+            'total_amount'    =>"500.00",
+            'ip_address'        => '127.0.0.1',
+            'date'  => "2025-05-14",
+            'status'     => "Credit",
+            'credit_amount'=>'500.00',
+            'debit_amount'=>''
+        ]);
+
+        DB::table('transation')->insert([
+            'cust_id'         => "2",
+            'total_amount'    =>"495.00",
+            'ip_address'        => '127.0.0.1',
+            'date'  => "2025-05-14",
+            'status'     => "Debit",
+            'credit_amount'=>'',
+            'debit_amount'=>'5.00'
+        ]);
+
+    }
+}
