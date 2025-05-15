54ad72a7ffb525f1a3bbe532df0298f22a95c8b8
bincybabu <bincysoja2017@gmail.com>
2025-05-15 10:03:10 +0530

    add postmanfiles

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -21,6 +21,19 @@
                 
                 @endif
               </div>
+              @if ($message = Session::get('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              <button
+                type="button"
+                class="close"
+                data-dismiss="alert"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+                <p>{{ $message }}</p>
+            </div>
+        @endif
               <div class="card-body">
                 <table id="datatablescustomerlist">
                   <thead>
